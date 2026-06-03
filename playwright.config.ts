3b9c66b2a56c01d1315087efefdfb36182586ce5
HenishTrada <tradahenish94@gmail.com>
2026-06-03 15:08:14 +0530

    [Update] : updating the existing checkBoxEvent.spec.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,8 +29,9 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-    headless : true
+    trace: 'on-all-retries',
+    headless : true,
+    screenshot : "only-on-failure",
   },
 
   /* Configure projects for major browsers */
--- tests/Day03/checkBoxChecked.spec.ts
+++ tests/Day03/checkBoxChecked.spec.ts
@@ -11,74 +11,53 @@ async function cookieHandler(page: Page) {
     }
 }
 
-test("Check Box Checked", async ({ page }) => {
-    await page.goto("https://www.ikea.com/in/en/");
+// test("Check Box Checked", async ({ page }) => {
+//     await page.goto("https://www.ikea.com/in/en/");
 
-    await cookieHandler(page);
+//     const cookiesBanner = page.locator(".ot-sdk-row");
 
-    const productCards = page.locator('.hri-teaser__content');
-    await productCards.first().waitFor();
+//     //using getByRole locator strategy
+//     await cookiesBanner.waitFor({ state: "visible", timeout: 5000 });
+//     await cookiesBanner.getByRole("button", { name: "Ok", exact: true }).click();
+//     await expect(cookiesBanner).toBeHidden({ timeout: 5000 });
 
-    await page.locator(".hri-compact-card__title").filter({ hasText: 'Outdoor furniture' }).click();
+//     // const productCards = page.locator('.hri-teaser__content');
+//     // await productCards.first().waitFor();
 
-    const heading = page.getByRole("heading", { name: "outdoor furniture", level: 1 });
-    await heading.waitFor({ state: "visible" });
+//     // await page.locator(".hri-compact-card__title").filter({ hasText: 'Outdoor furniture' }).click();
 
-    //using getByTestId locator and check() to check the checkbox
-    const checkBox_TopSeller = page.locator('.plp-filter-side-bar').getByTestId("plp-checkbox-label-TOP_SELLER");
-    await checkBox_TopSeller.check();
+//     // const heading = page.getByRole("heading", { name: "outdoor furniture", level: 1 });
+//     // await heading.waitFor({ state: "visible" });
+//     const searchPlaceholder = await page.getByPlaceholder('What are you looking for?');
+//     await searchPlaceholder.fill("old furniture");
+//     await expect(searchPlaceholder).toHaveValue("old furniture");
+//     await searchPlaceholder.press("Enter");
 
-    //using toBeChecked assertion to verify that the checkbox is checked
-    await expect(checkBox_TopSeller).toBeChecked();
-    await page.waitForTimeout(2000);
+//     //using getByTestId locator and check() to check the checkbox
+//     const checkBox_TopSeller = page.locator('.plp-filter-side-bar').getByTestId("plp-checkbox-label-TOP_SELLER");
+//     await checkBox_TopSeller.check();
 
-    const checkBox_lastChance = page.locator('.plp-filter-side-bar').getByTestId("plp-checkbox-label-LAST_CHANCE");
-    await checkBox_lastChance.check();
+//     //using toBeChecked assertion to verify that the checkbox is checked
+//     await expect(checkBox_TopSeller).toBeChecked();
+//     await page.waitForTimeout(2000);
 
-
-    await expect(checkBox_lastChance).toBeChecked();
-    await checkBox_TopSeller.uncheck();
-
-    await expect(checkBox_TopSeller).not.toBeChecked();
-
-    await page.waitForTimeout(2000);
-
-});
-
-test("Check Box Checked - Negative Scenario", async ({ page }) => {
-    await page.goto("https://www.ikea.com/in/en/");
-
-    await cookieHandler(page);
-
-    const productCards = page.locator('.hri-teaser__content');
-    await productCards.first().waitFor();
-
-    await page.locator(".hri-compact-card__title").filter({ hasText: 'Outdoor furniture' }).click();
-
-    const heading = page.getByRole("heading", { name: "outdoor furniture", level: 1 });
-    await heading.waitFor({ state: "visible" });
-
-    //using getByTestId locator and check() to check the checkbox
-    const checkBox_TopSeller = page.locator('.plp-filter-side-bar').getByTestId("plp-checkbox-label-TOP_SELLER");
-    await checkBox_TopSeller.check();
-
-    //using toBeChecked assertion to verify that the checkbox is checked
-    await expect(checkBox_TopSeller).toBeChecked();
-    await page.waitForTimeout(2000);
-
-    const checkBox_specialPrice = page.locator('.plp-filter-side-bar').getByTestId("plp-checkbox-label-SPECIAL_PRICE");
-    await checkBox_specialPrice.check();
+//     const checkBox_lastChance = page.locator('.plp-filter-side-bar').getByTestId("plp-checkbox-label-LAST_CHANCE");
+//     await checkBox_lastChance.check();
 
 
-    await expect(checkBox_specialPrice).not.toBeChecked();
+//     await expect(checkBox_lastChance).toBeChecked();
+//     await checkBox_TopSeller.uncheck();
 
-    await page.waitForTimeout(2000);
-});
+//     await expect(checkBox_TopSeller).not.toBeChecked();
+
+//     await page.waitForTimeout(2000);
+
+// });
 
 test("Check Box Checked - 2", async ({ page }) => {
     await page.goto("https://www.flipkart.com/");
 
-     const loginPop = page.locator(".q7ywiQ");
+    const loginPop = page.locator(".q7ywiQ");
 
     if (await loginPop.isVisible()) {
         await loginPop.getByRole('button', { name: '✕' }).click();
