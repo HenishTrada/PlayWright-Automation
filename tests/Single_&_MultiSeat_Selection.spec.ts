4c51627eb1fa52d227c87460c00a1bf5d1015845
HenishTrada <tradahenish94@gmail.com>
2026-06-11 18:27:52 +0530

    [Add] : add ixigo home and login

--- tests/Single_&_MultiSeat_Selection.spec.ts
+++ tests/Single_&_MultiSeat_Selection.spec.ts
@@ -29,7 +29,7 @@ test.describe("Bus Booking Flow", () => {
     await BusSearch.pressSearchButton();
   });
   
-  test.only("Singel seat selection", {tag: '@regression'}, async ({page}) => {
+  test("Singel seat selection", {tag: '@regression'}, async ({page}) => {
     test.setTimeout(600000);
     const BusFilter = new BusSelectionPage(page);
     const PassengerInfo = new PassengerInfoPage(page);
